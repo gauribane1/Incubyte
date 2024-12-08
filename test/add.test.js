a103e0836e9e543caa14386d3b905e923be42fea
gauribane1 <gauri.b@sphinxworldbiz.co>
2024-12-08 10:26:58 +0530

    test cases for empty string and return single number if one number is passed

---
+++ test/add.test.js
@@ -0,0 +1,10 @@
+const add = require("../add");
+describe("add function", () => {
+  test("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  test("should return the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+});
